c78afa86359c9a138ea756a414768baec050aa4e
Elliot Wood <43952853+ElliotWood13@users.noreply.github.com>
2023-04-21 20:26:04 +0100

    Continue stake functionality

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -12,9 +12,16 @@ contract Staker {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  mapping ( address => uint256 ) public balances;
   uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 30 seconds;
+  bool openForWithdraw = false;
+  mapping ( address => uint256 ) public balances;
   event Stake(address, uint256);
+
+  modifier canWithdraw {
+    require(openForWithdraw == true);
+    _;
+  }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
@@ -24,19 +31,22 @@ contract Staker {
     emit Stake(msg.sender, msg.value);
   }
 
-  //   function stake(uint256 _amount) public payable {
-  //   console.log('amount', _amount);
-  //   emit Stake(msg.sender, _amount);
-  //   balances[msg.sender] = balances[msg.sender] += _amount;
-  // }
-
-
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
 
+  function execute() public {
+    if (address(this).balance >= threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+    } else {
+      openForWithdraw = true;
+    }
+  }
 
   // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
 
+  function withdraw() public canWithdraw {
+    
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
 
